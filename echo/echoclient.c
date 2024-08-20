f5de17e0fd2b01cb8da42c1194090c304339cfdb
emilyyy9876 <strongwomankky@gmail.com>
2024-08-20 19:40:23 +0900

    echo 완성

--- echo/echoclient.c
+++ echo/echoclient.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
     if (argc != 3)
     {
         // 사용법이 잘못되었을 때의 에러 메시지
-        fprint(stderr, "usage: %s <host> <port>\n", argv[0]);
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
         // 프로그램을 종료
         exit(0);
     }
@@ -24,18 +24,18 @@ int main(int argc, char **argv)
     // 두 번째 인자로 포트 번호를 받아오기
     port = argv[2];
 
-    // 서버에 연결하고, 소켓 디스크립터를 돌려받기
-    clientfd = Open_clientfd(host, port);
+    // 1. 서버에 연결하고, 소켓 디스크립터를 돌려받기 (주어진 호스트, 포트번호를 가지)
+    clientfd = open_clientfd(host, port);
     // RIO 구조체를 초기화하여 버퍼링된 읽기를 준비하기
     Rio_readinitb(&rio, clientfd);
 
-    // 표준 입력(stdin)으로부터 한 줄씩 읽어옵니다.
+    // 표준 입력(stdin)으로부터 한 줄씩 읽어오기
     while (Fgets(buf, MAXLINE, stdin) != NULL)
     {
         // 서버로 데이터를 전송하기
         Rio_writen(clientfd, buf, strlen(buf));
         // 서버로부터 응답을 한 줄씩 읽어오기
-        rio_readlineb(&rio, buf, MAXLINE);
+        Rio_readlineb(&rio, buf, MAXLINE);
         // 받은 응답을 표준 출력(stdout)에 출력하기
         Fputs(buf, stdout);
     }
--- echo/echoserver.c
+++ echo/echoserver.c
@@ -2,8 +2,27 @@
 #include "csapp.h"
 
 // echo 함수를 선언하기(이 함수는 클라이언트와의 연결에서 데이터를 수신하고, 다시 클라이언트로 전송함)
-void echo(int connfd);
 
+void echo(int connfd)
+{
+    // 읽어들인 데이터의 크기를 저장할 변수
+    size_t n;
+    // 데이터를 저장할 버퍼
+    char buf[MAXLINE];
+    // RIO 구조체(버퍼링된 I/O를 관리하는 구조체)
+    rio_t rio;
+
+    // RIO 구조체를 초기화하여, connfd로부터 버퍼링된 읽기를 준비하기
+    rio_readinitb(&rio, connfd);
+    // 클라이언트로부터 한 줄씩 데이터를 읽어오기
+    while ((n = rio_readlineb(&rio, buf, MAXLINE)) != 0)
+    {
+        // 읽어들인 데이터의 크기를 확인하고, 그 크기를 출력하기
+        printf("server received %d bytes\n", (int)n);
+        // 읽어들인 데이터를 클라이언트에게 다시 전송하기
+        Rio_writen(connfd, buf, n);
+    }
+}
 // 서버 소켓을 수신 대기 상태로 설정하고, 클라이언트의 연결을 받아들이는 코드
 int main(int argc, char **argv)
 {
@@ -29,7 +48,7 @@ int main(int argc, char **argv)
     }
 
     // 지정된 포트에서 수신 대기 소켓을 열기
-    listenfd = Open_listenfd(argv[1]);
+    listenfd = open_listenfd(argv[1]);
     // 무한 루프를 돌며 클라이언트의 연결을 기다리기
     while (1)
     {
